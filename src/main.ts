ed43e62c635fa4b3d357ff3173ecbfc40c32f5f5
s2000promax <s2000promax@gmail.com>
2023-02-04 19:54:13 +0400

    feat: add CORS

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ const start = async () => {
     dotenv.config();
     const PORT = process.env.PORT || process.env.USER_PORT || 5000;
     const app = await NestFactory.create(AppModule)
+    app.enableCors();
 
     await app.listen(PORT, () => console.log(`Server has been started on port ${PORT}`));
   } catch (e) {
