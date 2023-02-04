8a44dfad961a62a50586277f761974cc45f98206
s2000promax <s2000promax@gmail.com>
2023-02-04 13:52:27 +0400

    fix: changed album schema

--- src/album/shema/album.shema.ts
+++ src/album/shema/album.shema.ts
@@ -1,11 +1,23 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { HydratedDocument } from 'mongoose';
-import * as mongoose from 'mongoose';
+import * as mongoose from 'mongoose'
 import { Track } from '../../track/shema/track.shema';
+
 export type AlbumDocument = HydratedDocument<Album>;
-export declare class Album {
+
+@Schema()
+export class Album {
+    @Prop()
     name: string;
+
+    @Prop()
     author: string;
+
+    @Prop()
     picture: string;
+
+    @Prop({type: [{type: mongoose.Schema.Types.ObjectId, ref: 'tracks'}]})
     tracks: Track[];
 }
-export declare const AlbumSchema: mongoose.Schema<Album, mongoose.Model<Album, any, any, any, any>, {}, {}, {}, {}, mongoose.DefaultSchemaOptions, Album>;
+
+export const AlbumSchema = SchemaFactory.createForClass(Album);
