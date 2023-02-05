159e87c7a1a60f61bc3c25f1f97241eb50fec2e8
s2000promax <s2000promax@gmail.com>
2023-02-05 23:01:00 +0400

    feat: add redux

---
+++ client/pages/_app.tsx
@@ -0,0 +1,8 @@
+import { AppProps } from 'next/app';
+import { wrapper } from '@/store';
+
+const WrappedApp: React.FC<AppProps> = ({Component, pageProps}) => (
+  <Component {...pageProps} />
+);
+
+export default wrapper.withRedux(WrappedApp);
