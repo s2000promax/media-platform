90308121eafcbf11fb6133f587c34f2ff1d42bcf
s2000promax <s2000promax@gmail.com>
2023-02-06 14:01:03 +0400

    fix: useDispatch

--- client/components/Player/ui/player.component.tsx
+++ client/components/Player/ui/player.component.tsx
@@ -4,6 +4,8 @@ import { Pause, PlayArrow, VolumeUp } from '@mui/icons-material';
 import styles from '../style/player.module.scss'
 import { ITrack } from '@/types/tracks';
 import { TrackProgress } from '@/components/TrackProgress';
+import { useTypeSelector } from '@/hooks/useTypeSelector';
+import { useActions } from '@/hooks/useActions';
 
 export const PlayerComponent: React.FC = () => {
   const track: ITrack = {
@@ -16,19 +18,29 @@ export const PlayerComponent: React.FC = () => {
     picture: 'http://localhost:5000/image/123.png',
     comments: []
   };
-const active = false;
+  const { active, volume, pause, currentTime, duration } = useTypeSelector(state => state.player)
+  const { playTrack, pauseTrack } = useActions();
+
+  const play = () => {
+    if (pause) {
+      playTrack();
+    } else {
+      pauseTrack();
+    }
+  }
+
   return (
     <div className={styles.player}>
-      <IconButton>
-        {active ? <Pause /> : <PlayArrow />}
+      <IconButton onClick={play}>
+        {!pause ? <Pause/> : <PlayArrow/>}
       </IconButton>
       <Grid container direction="column" width="200px" margin="20px 0">
         <Typography>{track.name}</Typography>
         <Typography style={{ fontSize: '12px', color: 'gray' }}>{track.artist}</Typography>
       </Grid>
-      <TrackProgress left={0} right={100} onChange={()=> ({})} />
-      <VolumeUp style={{ marginLeft: 'auto' }} />
-      <TrackProgress left={0} right={100} onChange={()=> ({})} />
+      <TrackProgress left={0} right={100} onChange={() => ({})}/>
+      <VolumeUp style={{ marginLeft: 'auto' }}/>
+      <TrackProgress left={0} right={100} onChange={() => ({})}/>
     </div>
   );
 };
